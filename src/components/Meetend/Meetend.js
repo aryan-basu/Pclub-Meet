3bcceee15940cc34486d1b647456bca0715a1023
Aryan basu <aryanbasu005@gmail.com>
2021-08-15 20:17:00 +0530

    meetend update

--- src/components/Meetend/Meetend.js
+++ src/components/Meetend/Meetend.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import Button from '@material-ui/core/Button'
+import '../Home/Home.css'
+import Header from '../Header/Header';
+import { useHistory, useLocation } from 'react-router-dom';
 const Meetend = () => {
+    const history = useHistory();
+    const handleback=()=>{
+        history.push('/');
+      }
     return (
         <div>
-            <h1>Meetend</h1>
+            <Header/>
+        <div className="home">
+            <h1>You left the meeting.</h1>
+            
+            <button classname="input-btn" onClick={handleback}>Back To Home screen</button>
+          
+        </div>
         </div>
     );
 }
