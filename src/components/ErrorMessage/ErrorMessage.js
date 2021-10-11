a475eda85a45cda0546682394283f540aaf180b8
imakish <makishtp@gmail.com>
2021-10-11 22:06:15 +0530

    more changes

--- src/components/ErrorMessage/ErrorMessage.js
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -2,7 +2,7 @@ const ErrorMessage = ({ children }) => {
     return (
       <div
         style={{
-          width: "98%",
+          width: "95%",
           padding: 10,
           marginBottom: 10,
           borderRadius: 4,
